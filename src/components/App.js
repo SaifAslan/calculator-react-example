3f46c4f4a3a71e17793bb0151ba2f9f71ed95609
sDominator007 <saif.abdalkader@yahoo.com>
2020-11-25 13:19:42 +0300

    starting files

--- src/components/App.js
+++
@@ -1,6 +0,0 @@
-import React from "react"
-function App() {
-  return(<h1>hello react</h1>)
-}
-
-export default App;
---
+++ src/components/App/index.js
@@ -0,0 +1,12 @@
+import React from "react"
+import "./App.css"
+function App() {
+  return(<div className="App">
+    <div className="top">4:43</div>
+    <div className="display">0</div>
+    <div className="buttons">AC</div>
+    <div className="botton">-</div>
+  </div>)
+}
+
+export default App;
